23f09f59791cd9435301d12abdc722f7cd685438
Shashank_S <shashanks9190@gmail.com>
2025-04-26 15:17:00 +0530

    home change to /

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
