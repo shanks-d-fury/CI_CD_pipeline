92ac165952d8b69b161a39ba11723183b273d354
Shashank_S <shashanks9190@gmail.com>
2025-04-27 16:51:44 +0530

    Icons Debug succsful { changed the /icon to just icon} it worked

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use((req, res, next) => {
-	console.log(`Request received: ${req.method} ${req.url}`);
-	next();
-});
+// app.use((req, res, next) => {
+// 	console.log(`Request received: ${req.method} ${req.url}`);
+// 	next();
+// });
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-	if (req.url.startsWith("/icon/")) {
-		console.log(`Static file not found: ${req.url}`);
-	}
-	next();
-});
+// the above and the below one is used to debug the icons folder in the ec2 instance
+// app.use((req, res, next) => {
+// 	if (req.url.startsWith("/icon/")) {
+// 		console.log(`Static file not found: ${req.url}`);
+// 	}
+// 	next();
+// });
 
 app.use(express.json());
 
